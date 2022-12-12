518fbde88e5069669a3d603acce80d2e1b0d8c25
gokul1630 <gokulgp3167@gmail.com>
2022-12-12 22:57:37 +0530

    Added Config Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/Auth.module';
+import { BookMarkModule } from './book-mark/book-mark.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    BookMarkModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
